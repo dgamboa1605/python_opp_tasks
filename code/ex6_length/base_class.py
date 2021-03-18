6c9fd41924f0bb956682a6347240bc08e9d5edbb
Dennis Gamboa <llkalulasll@gmail.com>
2021-03-18 16:03:24 -0400

    ex6 length task

---
+++ code/ex6_length/base_class.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class BaseClass(ABC):
+
+    @abstractmethod
+    def calculate_length(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def validate_input(self):
+        raise NotImplementedError
---
+++ code/ex6_length/calculate_length.py
@@ -0,0 +1,5 @@
+class CalculateLength:
+
+    @staticmethod
+    def calculate(input_object):
+        return input_object.calculate_length()
