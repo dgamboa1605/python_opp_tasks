e22a90504d79a9251b858cd8018d985dd1747eb6
Dennis Gamboa <llkalulasll@gmail.com>
2021-03-19 08:36:41 -0400

    update helper

--- code/ex6_length/calculate_length.py
+++ code/ex6_length/calculate_length.py
@@ -1,5 +1,2 @@
-class CalculateLength:
-
-    @staticmethod
-    def calculate(input_object):
+def calculate_length(input_object):
         return input_object.calculate_length()
--- code/ex6_length/main.py
+++ code/ex6_length/main.py
@@ -1,12 +1,12 @@
 from string_class import StringClass
 from list_class import ListClass
 from dictionary_class import DictionaryClass
-from calculate_length import CalculateLength
+from calculate_length import calculate_length
 
 s = StringClass("123345")
 l = ListClass([1,2,3,4])
 d = DictionaryClass({"1":1, "2":2})
 
-print(CalculateLength.calculate(s))
-print(CalculateLength.calculate(l))
-print(CalculateLength.calculate(d))
+print(calculate_length(s))
+print(calculate_length(l))
+print(calculate_length(d))
