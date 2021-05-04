40724d565e8782025a0851ae7569aaf63187c47e
Dennis Gamboa <llkalulasll@gmail.com>
2021-05-04 11:55:12 -0400

    added readme file and argument in order to run the game

---
+++ tasks/ex11_final_project/README.md
@@ -0,0 +1,102 @@
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/llgamboll/python_opp_tasks">
+  </a>
+
+  <h3 align="center">Final Project - Pokemon Game</h3>
+</p>
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+
+The final project is based on the Pokémon game, it is a text-based computer game.
+basically you need to move around of the map and make sure if you are in the Grass section 
+and write the command "catch" in order to capture a Pokémon.
+
+By default, you have 10 pokeballs, and your first Pokémon is a pikachu, you can use a pikachu to make battles 
+and capture a Pokémon when it is weakened. 
+To move for the map you need to use cardinal points "east", "west", "north" and "south".
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+you need to know about the arguments (cli), 
+this is a series of commands that you need to send by console to run the game.
+* args
+  ```sh
+  python3 play_game.py --player-id 0 --map-dimension 10
+  ```
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone git@github.com:llgamboll/python_opp_tasks.git
+   ```
+2. Create Virtual Environment
+   ```sh
+   python3 -m venv /path/to/new/virtual/environment
+   ```
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+After to clone and create virtual environment you need to move to play_game.py and run the following command.
+
+```sh
+  python3 play_game.py --player-id 0 --map-dimension 10
+```
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learned, inspire, and create. 
+Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+<!-- CONTACT -->
+## Contact
+
+Dennis Gamboa - llkalulasll@gmail.com
+
+Project Link: [python_opp_tasks](https://github.com/llgamboll/python_opp_tasks)
+
--- tasks/ex11_final_project/play_game.py
+++ tasks/ex11_final_project/play_game.py
@@ -1,4 +1,5 @@
 import random
+import argparse
 
 from tasks.ex11_final_project.pokemon.pikachu import Pikachu
 from tasks.ex11_final_project.utils import print_description, print_help, RANDOM_PHRASES
@@ -7,34 +8,48 @@ from tasks.ex11_final_project.map import Map
 from tasks.ex11_final_project.items.pokeball import Pokeball
 from tasks.ex11_final_project.items.banana import Banana
 
-print_description()
-player = Player(0)
-map = Map(10, 10)
 
-pikachu = Pikachu()
+def main(player_id, map_dimension, pokeball, banana):
+    print_description()
+    player = Player(player_id)
+    map = Map(map_dimension, map_dimension)
 
-player.backpack.new_item(Pokeball(amount=10))
-player.backpack.new_item(Banana(amount=10))
-player.backpack.add_pokemon(pikachu)
+    pikachu = Pikachu()
 
-while True:
-    text_value = input('> ')
-    if text_value == 'exit':
-        print('Bye')
-        break
-    elif text_value == 'backpack':
-        print(player.backpack)
-    elif text_value == 'help':
-        print_help()
-    elif text_value == 'west' or text_value == 'east' or text_value == 'south' or text_value == 'north':
-        player.go_to(text_value)
-        map.draw(player)
-    elif text_value == 'catch':
-        if map.get_player_position(player):
-            pokemon = map.get_pokemon_of_player(player)
-            map.get_pokemon_attack(pokemon)
-            map.catch_pokemon(player)
-    elif text_value == 'map':
-        map.draw(player)
-    else:
-        print(RANDOM_PHRASES[random.randint(0, 8)].format(text_value))
+    player.backpack.new_item(Pokeball(amount=pokeball))
+    player.backpack.new_item(Banana(amount=banana))
+    player.backpack.add_pokemon(pikachu)
+
+    while True:
+        text_value = input('> ')
+        if text_value == 'exit':
+            print('Bye')
+            break
+        elif text_value == 'backpack':
+            print(player.backpack)
+        elif text_value == 'help':
+            print_help()
+        elif text_value == 'west' or text_value == 'east' or text_value == 'south' or text_value == 'north':
+            player.go_to(text_value)
+            map.draw(player)
+        elif text_value == 'catch':
+            if map.get_player_position(player):
+                pokemon = map.get_pokemon_of_player(player)
+                map.get_pokemon_attack(pokemon)
+                map.catch_pokemon(player)
+        elif text_value == 'map':
+            map.draw(player)
+        else:
+            print(RANDOM_PHRASES[random.randint(0, 8)].format(text_value))
+
+
+if __name__ == "__main__":
+    arg = argparse.ArgumentParser()
+    arg.add_argument('--player-id', required=True, help='player id to start the game', type=int)
+    arg.add_argument('--map-dimension', required=True, help='map dimension e.g. 10 and map will be 10X10', type=int)
+    arg.add_argument('--pokeball', default=10, help='amount of pokeball', type=int)
+    arg.add_argument('--banana', default=10, help='amount of banana', type=int)
+
+    args = arg.parse_args()
+
+    main(player_id=args.player_id, map_dimension=args.map_dimension, pokeball=args.pokeball, banana=args.banana)
